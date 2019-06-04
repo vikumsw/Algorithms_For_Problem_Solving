2ba923a95423a1e77292b8490653d9111e096538
amald <amald@gmail.com>
2019-06-04 22:04:32 +0800

    Fibonacci simplified

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -2,41 +2,33 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Fibonacci {
-    public static int cal(int index){
-        switch (index){
-            case 0:
-                return 0;
-            case 1:
-                return 1;
-                default:
-                    return cal(index -1) + cal(index -2);
-        }
-
-    }
-
-    public static int calFibLoop(int index){
-        switch (index){
+    public static int cal(int index) {
+        switch (index) {
             case 0:
                 return 0;
             case 1:
                 return 1;
             default:
-                int valn1   = 1;
-                int valn2   = 0;
-                int out     = 0;
-
-                for(int i = 1; i < index; i++){
-                    out     = valn1 + valn2;
-                    valn2   = valn1;
-                    valn1   = out;
-                }
-                return out;
+                return cal(index - 1) + cal(index - 2);
         }
+
+    }
+
+    public static int calFibLoop(int index) {
+            int valn1 = 0;
+            int valn2 = 1;
+            for (int i = 0; i < index; i++) {
+                int out = valn1 + valn2;
+                valn2 = valn1;
+                valn1 = out;
+            }
+            return valn1;
     }
 
     @Test
-    public void fibonacci(){
+    public void fibonacci() {
         Assert.assertEquals(3, cal(4));
+        Assert.assertEquals(2, calFibLoop(3));
         Assert.assertEquals(3, calFibLoop(4));
         Assert.assertEquals(cal(10), calFibLoop(10));
     }
