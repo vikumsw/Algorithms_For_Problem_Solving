b4ca323ba77eceff4f88895af295f83a4d50f834
Wijesinghe <vikumsw@gmail.com>
2021-05-01 21:53:49 +0530

    Adding algo solution

--- src/main/python/topKFrequent1.py
+++ src/main/python/topKFrequent1.py
@@ -1,7 +1,22 @@
 class Solution:
-    def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        word_counts = collections.Counter(words) 
-        print(word_counts)
-        arr = [(c,w) for w,c in word_counts.items()]
-        arr.sort(key=lambda item: (-item[0], item[1]))
-        return list(map(lambda x:x[1],arr))[:k]
+    def mySqrt(self, x: int) -> int:
+        if x==0:
+            return 0
+        elif x <= 3:
+            return 1
+        
+        l = 2
+        h = x/2
+        
+        while l<=h:
+            m = l + (h-l)//2
+            p2 = m**2
+            p3 = (m+1)**2
+            if p2==x or (p2<x<p3):
+                return int(m)
+            elif p2>x:
+                h = m-1
+            else:
+                l = m+1
+            
+        return -1
