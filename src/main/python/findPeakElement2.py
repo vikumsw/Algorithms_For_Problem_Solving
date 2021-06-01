5d961293307cc0bdc25c545d52eb37b3e2f2a029
Vikum Sri Wijesinghe <vikumsw@gmail.com>
2021-06-01 09:38:53 +0530

    Update findPeakElement2.py

--- src/main/python/findPeakElement2.py
+++ src/main/python/findPeakElement2.py
@@ -6,6 +6,6 @@ class Solution:
             mid = lo + (hi - lo) // 2
             if nums[mid]>nums[mid+1]:
                 hi = mid
-            elif nums[mid]<nums[mid+1]:
+            else:
                 lo = mid+1
         return lo
