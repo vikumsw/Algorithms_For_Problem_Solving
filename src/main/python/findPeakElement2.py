509c135950abb780f0403bd2bce464a0acdc9b13
Vikum Sri Wijesinghe <vikumsw@gmail.com>
2021-06-01 09:27:03 +0530

    Update findPeakElement2.py

--- src/main/python/findPeakElement2.py
+++ src/main/python/findPeakElement2.py
@@ -7,15 +7,11 @@ class Solution:
         
         lo = 0
         hi = l-1
-        while lo<=hi:
+        while lo<hi:
             mid = lo + (hi - lo) // 2
-            if (0<mid<l-1 and nums[mid-1]<nums[mid]>nums[mid+1]) or (mid==0 and nums[mid] > nums[mid+1]) or (mid==l-1 and nums[mid]>nums[mid-1]):
-                return mid
-            elif (mid>0 and nums[mid-1]>nums[mid]>nums[mid+1]) or (mid==l-1 and nums[mid] < nums[mid-1]):
-                hi = mid - 1
-            elif (nums[mid-1]<nums[mid]<nums[mid+1]) or (mid==0 and nums[mid] < nums[mid+1]):
+            if nums[mid]>nums[mid+1]:
+                hi = mid
+            elif nums[mid]<nums[mid+1]:
                 lo = mid+1
-            else:
-                hi = mid - 1
             
-        return -1
+        return lo
