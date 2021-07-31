bd5069793b0fc77b02b5afc231a6692a5352c40f
Vikum Sri Wijesinghe <vikumsw@gmail.com>
2021-07-31 12:40:00 +0530

    Create detectCycle1.py

---
+++ src/main/python/detectCycle1.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        
+        if head==None: return None
+        
+        cur = head
+        visited = set()
+        visited.add(cur)
+        
+        while True:
+            if cur.next == None: return None
+            else: cur = cur.next
+            
+            if cur in visited:return cur
+            else:visited.add(cur)
