167ce4cc84a507f4d8acd754de01ee807a7417eb
Wijesinghe <vikumsw@gmail.com>
2021-08-19 21:53:49 +0530

    Adding algo solution

--- src/main/python/interchangeableRectangles.py
+++ src/main/python/interchangeableRectangles.py
@@ -6,9 +6,6 @@ class Solution:
             ratio = rect[0]/rect[1]
             ratio_map[ratio] +=1 
             
-            
-        
-        print(ratio_map)
         for ratio_val in ratio_map.values():
             if ratio_val>1:
                 count += math.factorial(ratio_val)/(2*math.factorial(ratio_val-2))
