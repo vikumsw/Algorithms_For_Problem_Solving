fc15805f03e564d6d198101ce74b2cd79fc96bc5
Wijesinghe <vikumw@lseg.com>
2021-08-30 21:53:49 +0530

    Adding algo solution

---
+++ src/main/python/interchangeableRectangles.py
@@ -0,0 +1,14 @@
+class Solution:
+    def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+        count = 0
+        ratio_map = DefaultDict(int)
+        for rect in rectangles:
+            ratio = rect[0]/rect[1]
+            ratio_map[ratio] +=1 
+        
+        print(ratio_map)
+        for ratio_val in ratio_map.values():
+            if ratio_val>1:
+                count += math.factorial(ratio_val)/(2*math.factorial(ratio_val-2))
+        
+        return int(count)
