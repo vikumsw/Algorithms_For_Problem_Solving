b5cbe3d52afc05ad025e4349f299d7fb3d235f15
Wijesinghe <vikumsw@gmail.com>
2021-08-31 21:53:49 +0530

    Adding algo solution

---
+++ src/main/python/finalValueAfterOperations.py
@@ -0,0 +1,8 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        X=0
+        for op in operations:
+            if '+' == op[1]:X+=1
+            else:X-=1
+        
+        return X
