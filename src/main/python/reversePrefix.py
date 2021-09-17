ad667acb00cdae5a74be70a779ec274ddb0a828b
Wijesinghe <vikumw@lseg.com>
2021-09-17 06:41:35 +0530

    ..

---
+++ src/main/python/reversePrefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        ll = len(word)
+        ind = -1
+        found = False
+        for i in range(ll):
+            if word[i]==ch:
+                found = True
+                ind = i
+                break
+        
+        ret = word
+        if found:
+            ret = word[:ind+1][::-1] + word[ind+1:]
+            
+        return ret
